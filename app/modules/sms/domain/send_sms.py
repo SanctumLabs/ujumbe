aca47f36690c7404a30cbec59e0873314a146aac
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-03-02 09:25:47 +0300

    chore(deps): kafka client dependency

--- app/modules/sms/domain/send_sms.py
+++ app/modules/sms/domain/send_sms.py
@@ -8,7 +8,10 @@ from app.infra.tasks.sms_sending_task import sms_sending_task
 
 def send_sms(request: SmsRequest):
     """
-    Use case that sends out an SMS
+    Use case that sends out an SMS.
+    We will first persist the SMS request, before sending it out. This will send an event to a broker
+    which will then publish the event to consumers(subscribers) which will handle saving the sms
+    request & then later send out the SMS
     """
     if not request:
         raise SendSmsException("Invalid sms request provided")
