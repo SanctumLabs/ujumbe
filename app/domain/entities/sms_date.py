b1b8a1f03db2a8f8fdc24add921bbcd162fbd2ef
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-04-04 06:09:51 +0300

    feat(entities): sms date entity

--- app/domain/entities/sms_date.py
+++ app/domain/entities/sms_date.py
@@ -1,10 +1,27 @@
+from typing import Union
 from datetime import datetime
 from dataclasses import dataclass
 from app.core.domain.entities.value_object import ValueObject
 
+DATE_PATTERN = "%d, %B %Y %H:%M:%S +0000"
+
 
 @dataclass
 class SmsDate(ValueObject):
-    date_created: datetime
-    date_sent: datetime
-    date_updated: datetime
+    date_created: Union[datetime, str]
+    date_sent: Union[datetime, str]
+    date_updated: Union[datetime, str]
+
+    def __post_init__(self):
+        created_date = datetime.strptime(self.date_created, DATE_PATTERN) if isinstance(self.date_created,
+                                                                                        str) else self.date_created
+        sent_date = datetime.strptime(self.date_sent, DATE_PATTERN) if isinstance(self.date_sent,
+                                                                                  str) else self.date_sent
+        updated_date = datetime.strptime(self.date_updated, DATE_PATTERN) if isinstance(self.date_updated,
+                                                                                        str) else self.date_updated
+
+        if created_date > sent_date:
+            raise ValueError(f"Created date: {self.date_created} cannot be after Sent Date: {self.date_sent}")
+
+        if created_date > updated_date:
+            raise ValueError(f"Created date: {self.date_created} cannot be after Date Updated: {self.date_updated}")
