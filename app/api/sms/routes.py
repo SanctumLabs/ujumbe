95c225eddd376be61866b6aaf308be15bbb40de6
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-09-14 14:26:16 +0300

    feat(tracing): setup tracing on sms routes

--- app/api/sms/routes.py
+++ app/api/sms/routes.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 from starlette import status
+from dependency_injector.wiring import inject
+from opentelemetry import trace
 from app.infra.logger import log as logger
-from .dto import SmsRequestDto, SmsCallbackRequestDto
-from ..dto import BadRequest, ApiResponse
 from app.core.domain.exceptions import AppException
 from app.domain.sms.submit_sms import SubmitSmsService
 from app.domain.sms.submit_sms_callback import SubmitSmsCallbackService
@@ -10,14 +10,17 @@ from app.config.di.dependency import dependency
 from app.domain.entities.sms import Sms
 from app.domain.entities.sms_callback import SmsCallback
 from app.config.di.container import ApplicationContainer
-from dependency_injector.wiring import inject
+from .dto import SmsRequestDto, SmsCallbackRequestDto
+from ..dto import BadRequest, ApiResponse
 
 router = APIRouter(prefix="/v1/sms", tags=["SMS"])
+tracer_name = "sms"
+tracer = trace.get_tracer(tracer_name)
 
 
 @router.post(
     path="/",
-    summary="Sends SMS",
+    summary="Sends SMS API that handles sending SMS messages to recipients",
     description="Sends an SMS request",
     response_model=ApiResponse,
 )
@@ -26,40 +29,48 @@ async def send_sms_api(
     payload: SmsRequestDto,
     submit_sms: SubmitSmsService = dependency(ApplicationContainer.domain.submit_sms),
 ):
+    """Send SMS API. This is a POST REST endpoint that accepts requests that meet the criteria defined by the schema
+    validation before sending a plain text sms
+
+    Args:
+        payload(SmsRequestDto): SMS Request payload
+        submit_sms(SubmitSmsService): Handles sms request payload and submits it for processing
+    Returns:
+        ApiResponse: returns a JSON response to a client.
     """
-    Send SMS API. This is a POST REST endpoint that accepts requests that meet the criteria defined by the
-    schema validation before sending a plain text sms
-    :return: JSON response to client
-    """
-    if not payload:
-        return BadRequest(message="No data provided")
+    with tracer.start_span(f"{tracer_name}.send"):
+        if not payload:
+            return BadRequest(message="No data provided")
 
-    try:
-        data = dict(
-            sender=payload.sender, recipient=payload.recipient, message=payload.message
-        )
-
-        sms = Sms.from_dict(data)
-
-        submit_sms.execute(sms)
-
-        return ApiResponse(
-            status=status.HTTP_200_OK, message="Sms sent out successfully"
-        )
-    except Exception as e:
-        logger.error(f"Failed to send sms to {payload} with error {e}")
-        if e is AppException:
-            return ApiResponse(
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                message="Failed to send SMS",
+        try:
+            data = dict(
+                sender=payload.sender,
+                recipient=payload.recipient,
+                message=payload.message,
             )
-        else:
-            return ApiResponse(status=status.HTTP_400_BAD_REQUEST, message=f"{e}")
+
+            sms = Sms.from_dict(data)
+
+            submit_sms.execute(sms)
+
+            return ApiResponse(
+                status=status.HTTP_200_OK, message="Sms sent out successfully"
+            )
+        except Exception as e:
+            logger.error(f"Failed to send sms to {payload} with error {e}")
+            if e is AppException:
+                return ApiResponse(
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    message="Failed to send SMS",
+                )
+            else:
+                return ApiResponse(status=status.HTTP_400_BAD_REQUEST, message=f"{e}")
 
 
 @router.post(
     path="/callback",
-    summary="SMS Callback to track delivery of sms",
+    summary="SMS Callback to track delivery of sent SMS. 3rd part clients communicate the deliver of an SMS with this "
+    "webhook",
     description="Receives callback from 3rd Party services to track the delivery status of messages",
     response_model=ApiResponse,
 )
@@ -70,28 +81,33 @@ async def sms_callback_api(
         ApplicationContainer.domain.submit_sms_callback
     ),
 ):
+    """SMS callback API. This is a POST REST endpoint that accepts sms callback requests that is used to track the
+    status of an initially delivered SMS message.
+    Args:
+        payload(SmsCallbackRequestDto): Callback Request DTO as provided by a client
+        submit_sms_callback(SubmitSmsCallbackService): service that handles processing of SMS Callback requests
+    Returns:
+        ApiResponse: JSON response to a client
     """
-    SMS callback API. This is a POST REST endpoint that accepts sms callback requests that is used to track the status
-    of an initially delivered SMS message.
-    :return: JSON response to client
-    """
-    if not payload:
-        return BadRequest(message="No data provided")
+    with tracer.start_span(f"{tracer_name}.callback"):
+        if not payload:
+            return BadRequest(message="No data provided")
 
-    try:
-        callback = SmsCallback.from_dict(payload.dict())
+        try:
+            callback = SmsCallback.from_dict(payload.dict())
 
-        submit_sms_callback.execute(callback)
+            submit_sms_callback.execute(callback)
 
-        return ApiResponse(
-            status=status.HTTP_202_ACCEPTED, message="Sms status received successfully"
-        )
-    except Exception as e:
-        logger.error(f"Failed to handle sms callback {payload} with error {e}")
-        if e is AppException:
             return ApiResponse(
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                message="Failed to handle SMS Callback",
+                status=status.HTTP_202_ACCEPTED,
+                message="Sms status received successfully",
             )
-        else:
-            return ApiResponse(status=status.HTTP_400_BAD_REQUEST, message=f"{e}")
+        except Exception as e:
+            logger.error(f"Failed to handle sms callback {payload} with error {e}")
+            if e is AppException:
+                return ApiResponse(
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    message="Failed to handle SMS Callback",
+                )
+            else:
+                return ApiResponse(status=status.HTTP_400_BAD_REQUEST, message=f"{e}")
